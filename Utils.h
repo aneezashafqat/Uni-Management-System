a1536e7245618aeaee0ce43fe79662e5ff7c5006
aneezashafqat <aneeeza.shafqat@gmail.com>
2026-05-04 03:13:48 +0500

    Update Utils.h
    proof reading

--- Utils.h
+++ Utils.h
@@ -3,19 +3,20 @@
 
 #include <string>
 
-void clearScreen();
-void pauseScreen();
-void printLine(char c = '-', int len = 60);
-void printHeader(const std::string& title);
-std::string trim(const std::string& s);
-void clearInput();
-int safeIntInput(const std::string& prompt, int minVal, int maxVal);
-double safeDoubleInput(const std::string& prompt, double minVal, double maxVal);
-bool isValidID(const std::string& id);
-bool isValidName(const std::string& name);
-bool isValidEmail(const std::string& email);
-bool isValidTimeSlot(const std::string& slot);
-bool confirmAction(const std::string& message);
+void clearScreen(); //clears screen 
+void pauseScreen(); //user presses enter to move on 
+void printLine(char c = '-', int len = 60); //for formatting prints line of repeated chars
+void printHeader(const std::string& title); //centers the title
+std::string trim(const std::string& s); //string manipulation removes spaces from start and end 
+void clearInput(); //clears for errors 
+int safeIntInput(const std::string& prompt, int minVal, int maxVal); //reads range for intengers (validation)
+double safeDoubleInput(const std::string& prompt, double minVal, double maxVal); //reads range for decimals (validation)
+bool isValidID(const std::string& id); // checks if ID only has nums
+bool isValidName(const std::string& name); //validates name
+bool isValidEmail(const std::string& email); //formats emails
+bool isValidTimeSlot(const std::string& slot); //formats time 
+bool confirmAction(const std::string& message); //yes before deletion or others
+//grade conversions
 double percentToGPA(double pct);
 std::string letterGrade(double pct);
 int safeInt(const std::string& s);

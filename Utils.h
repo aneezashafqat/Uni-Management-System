1d1fd0be993ee88e96483b4f699bdd67b0c0f54d
aneezashafqat <aneeeza.shafqat@gmail.com>
2026-05-08 15:57:18 +0500

    Update Utils.h
    updates functions

--- Utils.h
+++ Utils.h
@@ -3,23 +3,34 @@
 
 #include <string>
 
-void clearScreen(); //clears screen 
-void pauseScreen(); //user presses enter to move on 
-void printLine(char c = '-', int len = 60); //for formatting prints line of repeated chars
-void printHeader(const std::string& title); //centers the title
-std::string trim(const std::string& s); //string manipulation removes spaces from start and end 
-void clearInput(); //clears for errors 
-int safeIntInput(const std::string& prompt, int minVal, int maxVal); //reads range for intengers (validation)
-double safeDoubleInput(const std::string& prompt, double minVal, double maxVal); //reads range for decimals (validation)
-bool isValidID(const std::string& id); // checks if ID only has nums
-bool isValidName(const std::string& name); //validates name
-bool isValidEmail(const std::string& email); //formats emails
-bool isValidTimeSlot(const std::string& slot); //formats time 
-bool confirmAction(const std::string& message); //yes before deletion or others
-//grade conversions
+void clearScreen();
+void pauseScreen();
+void printLine(char c = '-', int len = 60);
+void printHeader(const std::string& title);
+
+std::string trim(const std::string& s);
+
+void clearInput();
+int safeIntInput(const std::string& prompt, int minVal, int maxVal);
+double safeDoubleInput(const std::string& prompt, double minVal, double maxVal);
+
+bool isValidID(const std::string& id);
+bool isValidName(const std::string& name);
+bool isValidEmail(const std::string& email);
+bool isValidTimeSlot(const std::string& slot);
+bool confirmAction(const std::string& message);
+
 double percentToGPA(double pct);
 std::string letterGrade(double pct);
+
 int safeInt(const std::string& s);
 double safeDouble(const std::string& s);
+
+std::string getValidatedID(const std::string& prompt);
+std::string getValidatedName(const std::string& prompt);
+std::string getValidatedEmail(const std::string& prompt);
+std::string getValidatedType(const std::string& prompt);
+
+int showMenu(const std::string& title, const std::string options[], int count);
 
 #endif
